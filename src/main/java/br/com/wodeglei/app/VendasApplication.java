fdabb616a7d188e4cf17a0470cd5f6adbe0acd48
Wodeglei Junior <wodejunior@gmail.com>
2025-10-15 22:17:11 -0300

    Primeira atualização!

---
+++ src/main/java/br/com/wodeglei/app/VendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.wodeglei.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+        sysout.println("Aplicação iniciada com sucesso!");
+    }
+}
