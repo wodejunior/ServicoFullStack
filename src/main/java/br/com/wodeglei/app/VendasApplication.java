a4ffe5f442c1a2a35be555555b84895c02861b79
Wodeglei Junior <wodejunior@gmail.com>
2025-10-15 22:18:47 -0300

    Primeira atualização Corrigida!

--- src/main/java/br/com/wodeglei/app/VendasApplication.java
+++ src/main/java/br/com/wodeglei/app/VendasApplication.java
@@ -8,6 +8,5 @@ public class VendasApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
-        sysout.println("Aplicação iniciada com sucesso!");
     }
 }
